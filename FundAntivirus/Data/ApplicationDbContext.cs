2111697bf5f4f35fa9b175dd7a37608b0e213757
jfar1988 <jfar1988@misena.edu.co>
2025-02-13 11:37:30 -0500

    feat(structure): add folder structure, implement user authentication and registration

---
+++ FundAntivirus/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using FundAntivirus.Models;
+namespace FundAntivirus.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ FundAntivirus/Models/User.cs
@@ -0,0 +1,16 @@
+namespace FundAntivirus.Models
+
+{
+    public class User
+    {
+        public int Id{get; set;}
+        public string UserName{get; set;}
+
+        public string Email{get; set;}
+
+        public string PasswordHash{get; set;}
+        
+        public string Role{get; set;} //Admin, User
+
+    }
+}
