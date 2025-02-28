4460a5e98a0d3690210e2bddbaaa463a6aefba61
jfar1988 <jfar1988@misena.edu.co>
2025-02-28 08:53:18 -0500

    Chore(.Gitignore): se modifica gitignore, credenciales del json y context

--- FundacionAntivirus/Data/AppDbContext.cs
+++ FundacionAntivirus/Data/AppDbContext.cs
@@ -34,7 +34,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=dbantivirus;Username=postgres;Password=123987");
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=Antivirus;Username=postgres;Password=123987");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
