a7606293c90c84eafb058b1095d14b2f9d88b9be
jfar1988 <jfar1988@misena.edu.co>
2025-03-04 19:54:31 -0500

    refactor: Organizar la configuración de relaciones en métodos separados(recomendaciones de la profe)

--- FundacionAntivirus/Data/AppDbContext.cs
+++ FundacionAntivirus/Data/AppDbContext.cs
@@ -7,98 +7,130 @@ namespace FundacionAntivirus.Data;
 
 public partial class AppDbContext : DbContext
 {
-    public AppDbContext()
-    {
-    }
+    public AppDbContext() { }
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
 
     public virtual DbSet<BootcampTopics> BootcampTopics { get; set; }
-
     public virtual DbSet<Bootcamps> Bootcamps { get; set; }
-
     public virtual DbSet<Categories> Categories { get; set; }
-
     public virtual DbSet<Institutions> Institutions { get; set; }
-
     public virtual DbSet<Opportunities> Opportunities { get; set; }
-
     public virtual DbSet<Topics> Topics { get; set; }
-
     public virtual DbSet<UserOpportunities> UserOpportunities { get; set; }
-
     public virtual DbSet<Users> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=localhost;Database=Antivirus;Username=postgres;Password=123987");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ConfigureBootcampTopics(modelBuilder);
+        ConfigureBootcamps(modelBuilder);
+        ConfigureCategories(modelBuilder);
+        ConfigureInstitutions(modelBuilder);
+        ConfigureOpportunities(modelBuilder);
+        ConfigureTopics(modelBuilder);
+        ConfigureUserOpportunities(modelBuilder);
+        ConfigureUsers(modelBuilder);
+        
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    private void ConfigureBootcampTopics(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<BootcampTopics>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("bootcamp_topics_pkey");
 
-            entity.HasOne(d => d.Bootcamp).WithMany(p => p.BootcampTopics).HasConstraintName("bootcamp_topics_bootcamp_id_fkey");
+            entity.HasOne(d => d.Bootcamp)
+                .WithMany(p => p.BootcampTopics)
+                .HasConstraintName("bootcamp_topics_bootcamp_id_fkey");
 
-            entity.HasOne(d => d.Topic).WithMany(p => p.BootcampTopics).HasConstraintName("bootcamp_topics_topic_id_fkey");
+            entity.HasOne(d => d.Topic)
+                .WithMany(p => p.BootcampTopics)
+                .HasConstraintName("bootcamp_topics_topic_id_fkey");
         });
+    }
 
+    private void ConfigureBootcamps(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Bootcamps>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("bootcamps_pkey");
 
-            entity.HasOne(d => d.Institution).WithMany(p => p.Bootcamps)
+            entity.HasOne(d => d.Institution)
+                .WithMany(p => p.Bootcamps)
                 .OnDelete(DeleteBehavior.SetNull)
                 .HasConstraintName("bootcamps_institution_id_fkey");
         });
+    }
 
+    private void ConfigureCategories(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Categories>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("categories_pkey");
         });
+    }
 
+    private void ConfigureInstitutions(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Institutions>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("institutions_pkey");
         });
+    }
 
+    private void ConfigureOpportunities(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Opportunities>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("opportunities_pkey");
 
-            entity.HasOne(d => d.Category).WithMany(p => p.Opportunities)
+            entity.HasOne(d => d.Category)
+                .WithMany(p => p.Opportunities)
                 .OnDelete(DeleteBehavior.SetNull)
                 .HasConstraintName("opportunities_category_id_fkey");
 
-            entity.HasOne(d => d.Institution).WithMany(p => p.Opportunities)
+            entity.HasOne(d => d.Institution)
+                .WithMany(p => p.Opportunities)
                 .OnDelete(DeleteBehavior.SetNull)
                 .HasConstraintName("opportunities_institution_id_fkey");
         });
+    }
 
+    private void ConfigureTopics(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Topics>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("topics_pkey");
         });
+    }
 
+    private void ConfigureUserOpportunities(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<UserOpportunities>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("user_opportunities_pkey");
 
-            entity.HasOne(d => d.Opportunity).WithMany(p => p.UserOpportunities).HasConstraintName("user_opportunities_opportunity_id_fkey");
+            entity.HasOne(d => d.Opportunity)
+                .WithMany(p => p.UserOpportunities)
+                .HasConstraintName("user_opportunities_opportunity_id_fkey");
 
-            entity.HasOne(d => d.User).WithMany(p => p.UserOpportunities).HasConstraintName("user_opportunities_user_id_fkey");
+            entity.HasOne(d => d.User)
+                .WithMany(p => p.UserOpportunities)
+                .HasConstraintName("user_opportunities_user_id_fkey");
         });
+    }
 
+    private void ConfigureUsers(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<Users>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("users_pkey");
         });
-
-        OnModelCreatingPartial(modelBuilder);
     }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
